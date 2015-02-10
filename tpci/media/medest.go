28c17558605af4ff55629af7538a34f446c7090d
crbrox <carlos.romerobrox@telefonica.com>
2015-02-10 17:22:31 +0100

    Añadir media desv estándar

---
+++ tpci/media/medest.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"math"
+)
+
+func medEst(xs []float64) (media float64, devEst float64) {
+	N := float64(len(xs))
+	sumX := 0.0
+	sumCdr := 0.0
+	for _, x := range xs {
+		sumX += x
+		sumCdr += x * x
+	}
+	media = sumX / N
+	devEst = math.Sqrt((sumCdr - sumX*sumX/N) / (N - 1))
+	return media, devEst
+}
---
+++ tpci/media/media.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/crbrox/exercises/basic"
+)
+
+func main() {
+	const Max = 80
+
+	var xs []float64
+	var media, dest float64
+	var N int
+
+	fmt.Println()
+	fmt.Println("\tCáculo de la media y la desviación estándar")
+	for {
+		basic.Input("\t¿Cuántos puntos? ", &N)
+		if N <= Max {
+			break
+		}
+	}
+	xs = make([]float64, N)
+	for i := 0; i < N; i++ {
+		basic.Input(fmt.Sprintf("%3d: ", i+1), &xs[i])
+	}
+	media, dest = medEst(xs)
+	fmt.Println()
+	fmt.Printf("\tPara %3d puntos Media = %8.4f Sigma = %8.4f\n", N, media, dest)
+}
