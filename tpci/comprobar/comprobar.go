a08e2e17f71a2924eea29ac39fb54beec9133e33
crbrox <carlos.romerobrox@telefonica.com>
2015-02-18 19:50:25 +0100

    Sacar literales a constantes

--- tpci/comprobar/comprobar.go
+++ tpci/comprobar/comprobar.go
@@ -11,6 +11,11 @@ import (
 	"github.com/crbrox/exercises/tpci/tpci"
 )
 
+const (
+	MEDIA      = 0.5
+	DESVIACION = 0.2887
+)
+
 func main() {
 	const N = 10000
 	const M = 1000
@@ -22,11 +27,11 @@ func main() {
 	//sacamos la media y la desviación
 	//en bloques de M elementos
 	fmt.Printf("%-8s %8s\n", "Media", "Desviación")
-	fmt.Printf("%2.6f %2.6f\n", 0.5, 0.2887)
+	fmt.Printf("%2.6f %2.6f\n", MEDIA, DESVIACION)
 	fmt.Println(strings.Repeat("=", 8), strings.Repeat("=", 8))
 	for j := 0; j < N; j += M {
 		m, d := tcpi.MedEst(numbers[j : j+M])
-		fmt.Printf("%2.6f %2.6f (%+2.6f) (%+2.6f)\n", m, d, m-0.5, d-0.2887)
+		fmt.Printf("%2.6f %2.6f (%+2.6f) (%+2.6f)\n", m, d, m-MEDIA, d-DESVIACION)
 	}
 
 }
