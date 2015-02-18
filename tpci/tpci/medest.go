24461fe16afef05a8cc5c8c5a1e3ffbbc94b42be
crbrox <carlos.romerobrox@telefonica.com>
2015-02-18 20:35:03 +0100

    Añadir funcion para generación gaussiana

--- tpci/tpci/medest.go
+++ tpci/tpci/medest.go
@@ -2,6 +2,7 @@ package tcpi
 
 import (
 	"math"
+	"math/rand"
 )
 
 func MedEst(xs []float64) (media float64, devEst float64) {
@@ -15,4 +16,15 @@ func MedEst(xs []float64) (media float64, devEst float64) {
 	media = sumX / N
 	devEst = math.Sqrt((sumCdr - sumX*sumX/N) / (N - 1))
 	return media, devEst
+}
+
+// Genera números aleatorios con una distribución gaussiana
+func Aleatg(media, sigma float64) float64 {
+	var (
+		sum float64
+	)
+	for i := 0; i < 12; i++ {
+		sum += rand.Float64()
+	}
+	return (sum-6)*sigma + media
 }
