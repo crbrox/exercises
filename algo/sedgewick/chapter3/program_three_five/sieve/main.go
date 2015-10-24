89e68ddb341bc8a7bd599ae5150686ef878037b3
crbrox <carlos.romerobrox@telefonica.com>
2015-10-24 14:19:23 +0200

    Dynamic size for sieve, as program 3.6 suggest

--- algo/sedgewick/chapter3/program_three_five/sieve/main.go
+++ algo/sedgewick/chapter3/program_three_five/sieve/main.go
@@ -4,13 +4,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
-const n = 1000
-
-var a [n]bool
+var a []bool
 
 func main() {
+	if len(os.Args) != 2 {
+		fatalError("Usage: " + os.Args[0] + " <max number to check>")
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fatalError(err)
+	}
+	if n < 2 {
+		fatalError(fmt.Sprint("it should be greater than 2, passed in ", n))
+	}
+	a = make([]bool, n)
 	a[0] = true // is non prime
 	a[1] = true // we start in 2
 	for i, isNonPrime := range a {
@@ -26,4 +37,8 @@ func main() {
 		}
 	}
 	fmt.Println()
+}
+func fatalError(x interface{}) {
+	fmt.Println(x)
+	os.Exit(-1)
 }
