7d1311eb3303e65453501448d9000802b6e7d06f
crbrox <carlos.romerobrox@telefonica.com>
2015-11-07 13:36:59 +0100

    LPI, 57 unix sock dgram, pasar a mayúsculas

---
+++ lpi/57_unix_sockets/dgram/client_us_dgram/client_us_dgram.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+const bufSize = 10 * 2
+const svSockPath = "/tmp/testgounixdgram"
+const clSockPathFormat = "/tmp/testgounixdgram-%d"
+
+func main() {
+	log.Println("Hello World!, unix socket dgram client")
+	clPath := fmt.Sprintf(clSockPathFormat, os.Getpid())
+	localAddr := &net.UnixAddr{Net: "unixgram", Name: clPath}
+	serverAddr := &net.UnixAddr{Net: "unixgram", Name: svSockPath}
+	//Esto es un simple bind, para poder recibir las respuestas
+	unixConn, err := net.ListenUnixgram("unixgram", localAddr)
+	// Hay que borrar el path al salir !
+	defer os.Remove(clPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, msg := range os.Args[1:] {
+		nw, err := unixConn.WriteTo([]byte(msg), serverAddr)
+		log.Printf("(error: %v) enviados %d bytes\n", err, nw)
+		if err != nil {
+			// No esperamos repuesta si el envio no ha ido bien
+			continue
+		}
+		resp := make([]byte, bufSize)
+		size, sender, err := unixConn.ReadFromUnix(resp)
+		log.Printf("(error:%v) recibido mensaje %q (%d) de %v\n", err, resp[:size], size, sender)
+	}
+}
---
+++ lpi/57_unix_sockets/dgram/server_us_dgram/server_us_dgram.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net"
+	"os"
+)
+
+const bufSize = 10
+const svSockPath = "/tmp/testgounixdgram"
+
+func main() {
+	log.Println("Hello World!, unix socket dgram server")
+	// Borra path si ya existe
+	os.Remove(svSockPath)
+	//Esto es un simple bind
+	unixConn, err := net.ListenUnixgram("unixgram", &net.UnixAddr{Net: "Unixgram", Name: svSockPath})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		msg := make([]byte, bufSize)
+		sender := &net.UnixAddr{}
+		size, sender, err := unixConn.ReadFromUnix(msg)
+		log.Printf("(error:%v) recibido mensaje %q (%d) de %v\n", err, msg[:size], size, sender)
+		if err != nil {
+			// No enviamos repuesta si no hemos recibido el mensaje bien
+			continue
+		}
+		resp := bytes.ToUpper(msg[:size])
+		nw, err := unixConn.WriteTo(resp, sender)
+		log.Printf("(error: %v) enivados %d bytes", err, nw)
+	}
+}
