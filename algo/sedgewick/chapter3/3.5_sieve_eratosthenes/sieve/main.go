660de0a6010347264b6a8fd6073a9a8afc1288d3
crbrox <carlos.romerobrox@telefonica.com>
2017-09-30 19:31:42 +0200

    Usa cte para tamaño y buffer para salida en sieve.

--- algo/sedgewick/chapter3/3.5_sieve_eratosthenes/sieve/main.go
+++ algo/sedgewick/chapter3/3.5_sieve_eratosthenes/sieve/main.go
@@ -3,25 +3,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
-var a []bool
+const n = 10e6
 
 func main() {
-	if len(os.Args) != 2 {
-		fatalError("Usage: " + os.Args[0] + " <max number to check>")
-	}
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fatalError(err)
-	}
-	if n < 2 {
-		fatalError(fmt.Sprint("it should be greater than 2, passed in ", n))
-	}
-	a = make([]bool, n)
+
+	/*
+		f, err := os.Create("./cpuprof")
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	*/
+
+	output := bufio.NewWriter(os.Stdout)
+
+	a := make([]bool, n)
+
 	a[0] = true // is non prime
 	a[1] = true // we start in 2
 	for i, isNonPrime := range a {
@@ -33,7 +38,7 @@ func main() {
 	}
 	for i, isNonPrime := range a {
 		if !isNonPrime { // is prime
-			fmt.Printf("%4d ", i)
+			fmt.Fprintf(output, "%4d ", i)
 		}
 	}
 	fmt.Println()
