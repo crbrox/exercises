b7d294c2acb55a668a1dbe85a4c7a91e7aaff9a5
crbrox <carlos.romerobrox@telefonica.com>
2017-09-30 19:32:47 +0200

    version de sieve con array estático

---
+++ algo/sedgewick/chapter3/3.5_sieve_eratosthenes/sieve2/main2.go
@@ -0,0 +1,49 @@
+// sieve eratosthenes
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+const n = 10e6
+
+var a [n]bool
+
+func main() {
+
+	f, err := os.Create("./cpuprof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	output := bufio.NewWriter(os.Stdout)
+
+	a[0] = true // is non prime
+	a[1] = true // we start in 2
+	for i, isNonPrime := range a {
+		if !isNonPrime { // is prime
+			for j := i; j*i < n; j++ {
+				a[j*i] = true // is non-prime
+			}
+		}
+	}
+	for i, isNonPrime := range a {
+		if !isNonPrime { // is prime
+			fmt.Fprintf(output, "%4d ", i)
+		}
+	}
+	fmt.Println()
+}
+func fatalError(x interface{}) {
+	fmt.Println(x)
+	os.Exit(-1)
+}
